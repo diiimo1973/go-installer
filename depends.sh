406f09084229700713881ff9fa0e22ca08aefb70
diiimo2k20 <diiimo2k20@gmail.com>
2020-11-05 10:32:36 +0100

    add scripts

---
+++ depends.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+## Install dev dependencies
+go get -v -u github.com/Clever/gitsem
+go get -v -u github.com/go-task/task/cmd/task
+go get -v -u github.com/golang/mock/mockgen
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+## check and set required arg 1 version
+if [[ -z "$1" ]]
+  then
+    echo "No version supplied"
+    exit 1
+fi
+VERSION=$1
+
+OS=linux
+ARCH=amd64
+
+wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+rm go$VERSION.$OS-$ARCH.tar.gz
